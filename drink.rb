58210667fd72f2427555297ccffab32325d30564
OdaAyakaHanabi <oda.ayaka720@gmail.com>
2024-08-29 11:19:15 +0900

    class_method

--- drink.rb
+++ drink.rb
@@ -1,18 +1,14 @@
-# requireを使ってmenu.rbを読み込んでください
 require "./menu"
 
-# Menuクラスを継承したDrinkクラスをつくってください
 class Drink < Menu
-     # amountというインスタンス変数を追加してください
   attr_accessor :amount
-
-     # infoメソッドを定義してください
+  
+  def initialize(name:, price:, amount:)
+    super(name: name, price: price)
+    self.amount = amount
+  end
+  
   def info
-    return "#{self.name}#{self.price}円(#{self.amount}mL)"
+    return "#{self.name} #{self.price}円 (#{self.amount}mL)"
   end
-
-  def calorie_info
-    return "#{self.name}は#{self.calorie}kcalです"
-  end
-
 end
--- food.rb
+++ food.rb
@@ -1,25 +1,18 @@
-# requireを使ってmenu.rbを読み込んでください
 require "./menu"
 
-# Menuクラスを継承したFoodクラスをつくってください
 class Food < Menu
-     # calorieというインスタンス変数を追加してください
-    attr_accessor :calorie
-
-     # initializeメソッドを定義してください
+  attr_accessor :calorie
+  
   def initialize(name:, price:, calorie:)
-    self.name = name
-    self.price = price
+    super(name: name, price: price)
     self.calorie = calorie
   end
-
-      # infoメソッドを定義してください
+  
   def info
-    return "#{self.name}#{self.price}円 (#{self.calorie}kcal)"
+    return "#{self.name} #{self.price}円 (#{self.calorie}kcal)"
   end
-     # calorie_infoメソッドを定義してください
+  
   def calorie_info
     return "#{self.name}は#{self.calorie}kcalです"
   end
 end
-#endをわすれてしまった
